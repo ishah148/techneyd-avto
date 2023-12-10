b345d5d02208a21615c22eb51d3b2bbd1583d325
ishah <ishah@intervale.ru>
2023-12-10 23:41:05 +0300

    upd

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 // vite.config.js
-import { defineConfig } from 'vite';
-import injectHTML from 'vite-plugin-html-inject';
+import { defineConfig } from "vite";
+import injectHTML from "vite-plugin-html-inject";
 
 export default defineConfig({
   plugins: [injectHTML()],
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: '@use "@/assets/scss/variables" as *;',
+        additionalData: '@use "/src/assets/scss/variables" as *;',
       },
     },
   },
